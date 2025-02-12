9ae76143c0478fe073ece908765737109d629146
SuranaDhruv7 <dhruvsurana829@gmail.com>
2025-02-12 16:30:20 +0530

    Number Guessing Game

--- Number_guessing_Game.py
+++ Number_guessing_Game.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 print("==== Welcome to the Number Guessing Game ====")
 def number_guessing_game():
     min_value = 1
@@ -8,6 +9,31 @@ def number_guessing_game():
     number_to_guess = np.random.randint(min_value, max_value)
     attempts = 0
 
+    print("Choose the difficulty level: ")
+    print("1. Easy (10 attempts)")
+    print("2. Medium (5 attempts)")
+    print("3. Hard (3 attempts)")
+    difficulty = input("Enter your choice: ")
+    if difficulty.isdigit():
+        difficulty = int(difficulty)
+    else:
+        print("Invalid choice. Please enter a number.")
+        return 
+    if difficulty == 1:
+        max_attempts = 10
+        print("Game Loading...")
+        time.sleep(3) 
+    elif difficulty == 2:
+        max_attempts = 5
+        print("Game Loading...")
+        time.sleep(3) 
+    elif difficulty == 3:
+        max_attempts = 3
+        print("Game Loading...")
+        time.sleep(3) 
+    else:
+        print("Invalid choice. Please try again.")
+        return
     while True:
         guess = input("Enter your number: ")
 
@@ -24,5 +50,7 @@ def number_guessing_game():
         else:
             print(f"Congratulations! You guessed the number in {attempts} attempts.")
             break
-        
+        if max_attempts and attempts >= max_attempts:
+            print(f"Sorry! You've run out of attempts. The number was {number_to_guess}.")
+            break
 number_guessing_game()
